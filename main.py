d26cae7c195a387a6aa126917675f020d6f294d6
donwany <theodondre@gmail.com>
2022-09-22 23:27:07 -0500

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from mathz.add import add, multiply
 
 if __name__ == "__main__":
-    print(add(1, 2))
-    print(multiply(2, 0))
+    print(f"Addition is: {add(1, 2)}")
+    print(f"Multiplication is: {multiply(2, 5)}")
